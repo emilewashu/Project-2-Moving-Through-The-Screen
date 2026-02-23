c8a6ea46d0c166a439c3540ae2f5be25f9403655
emilewashu <112423708+emilewashu@users.noreply.github.com>
2026-02-23 16:12:28 -0600

    Added images and gif

--- sketch.js
+++ sketch.js
@@ -1,6 +1,18 @@
+let runnerGif;
+let bg;
+
 function setup() {
-  createCanvas(5000, 3000);
-  background("#da05b0");
+  createCanvas(windowWidth, windowHeight);
+  bg = loadImage("assets/pixelpark.jpg");
+  createRunner();
 }
 
-function draw() {}
+function draw() {
+  background(bg);
+}
+
+function createRunner() {
+  runnerGif = createImg("assets/run.gif");
+  runnerGif.position(width / 2 - 50, height / 2);
+  runnerGif.size(100, 100);
+}
