46461f633fa59cda19dda981431f89a7513f2714
emilewashu <112423708+emilewashu@users.noreply.github.com>
2026-02-27 14:05:28 -0600

    Added first half of images to collage page

--- sketch.js
+++ sketch.js
@@ -1,18 +1,85 @@
-let runnerGif;
-let bg;
+let sky, sun, birds, barcrowd, barwindow, facade;
+let startTime;
+
+let sunSize = 300;
+let scrollY = 0;
+const collageWidth = 1200;
+const collageHeight = 5000;
+let growing = true;
+
+function preload() {
+  sky = loadImage("assets/sky.png");
+  sun = loadImage("assets/cornersun.png");
+  birds = loadImage("assets/birds.gif");
+  barwindow = loadImage("assets/window.png");
+  facade = loadImage("assets/newfacade.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bg = loadImage("assets/pixelpark.jpg");
-  createRunner();
+  pixelDensity(1);
+  startTime = millis();
 }
 
 function draw() {
-  background(bg);
+  background(0);
+  let centerX = (width - collageWidth) / 2;
+
+  /**
+   * SKY SCENE
+   */
+
+  //sky
+  image(sky, centerX, -scrollY, collageWidth, 1200);
+
+  //sun
+  let sunX = centerX + collageWidth - sunSize;
+  image(sun, sunX, -scrollY, sunSize, sunSize);
+  if (growing) {
+    sunSize += 1;
+    if (sunSize >= 600) growing = false;
+  } else {
+    sunSize -= 1;
+    if (sunSize <= 300) growing = true;
+  }
+
+  //birds
+  let duration = 15000;
+  let elapsed = (millis() - startTime) % duration;
+
+  let progress = elapsed / duration;
+  let x = lerp(width, -200, progress);
+
+  image(birds, x, -scrollY + 200, 400, 400);
+
+  //bar
+
+  image(barwindow, centerX - 10, -scrollY + 950, collageWidth + 10, 250);
+
+  /**
+   * TRANSITION
+   */
+
+  //facade
+  image(facade, centerX, -scrollY + 1200, collageWidth, 900);
+
+  //canvas border
+  // noFill();
+  // stroke(255);
+  // strokeWeight(2);
+  // rect(centerX, -scrollY, collageWidth, collageHeight);
 }
 
-function createRunner() {
-  runnerGif = createImg("assets/run.gif");
-  runnerGif.position(width / 2 - 50, height / 2);
-  runnerGif.size(100, 100);
+function mouseWheel(event) {
+  scrollY += event.delta;
+  scrollY = constrain(scrollY, 0, collageHeight - height);
+  return false;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseClicked() {
+  console.log("Mouse clicked at: " + mouseX + ", " + mouseY);
 }
